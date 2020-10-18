92e8a5d16a0a19c5ff630e894ee64e7ecf5367c0
brandoncaulfield <19891236+brandoncaulfield@users.noreply.github.com>
2020-10-18 14:06:06 +0100

    Added new text with test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import image from './Hello_there.jpg'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="my_div">
+      <h1>General Kenobi meets Grevious</h1>
+      <img src={image}></img>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders h1 text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  const textElement = getByText(/General Kenobi meets Grevious/i);
+  expect(textElement).toBeInTheDocument();
+})
+
+
